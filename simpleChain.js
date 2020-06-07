ad3657e73187ba2d39bef76b042d96a4a64bab15
Cipher2472 <ayushtiwari5747462@gmail.com>
2020-06-07 18:43:24 +0530

    Implemented block data models

--- simpleChain.js
+++ simpleChain.js
@@ -1,17 +1,29 @@
+const SHA256=require('crypto-js/sha256');
 class Block{
   constructor(data){
     this.hash="";
     this.height=0;
     this.body=data;
-    this.time=0;
+    this.timestamp=0;
     this.previousblockhash=0;
   }
 }
 class Blockchain{
   constructor(){
     this.chain=[];
+    this.addBlock(this.createGenesisBlock());
+  }
+  createGenesisBlock(){
+    let firstBlock= new Block('Genesis Block');
+    return firstBlock;
   }
   addBlock(newBlock){
+    newBlock.height=this.chain.length;
+    newBlock.timestamp=new Date().getTime().toString().slice(0,-3);
+    if(this.chain.length>0){
+      newBlock.previousblockhash=this.chain[this.chain.length-1].hash;
+    }
+    newBlock.hash=SHA256(JSON.stringify(newBlock)).toString();
     this.chain.push(newBlock);
   }
 }
