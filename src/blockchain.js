9453551bde7de84f0a9feab551051a16a4e07563
Cipher2472 <ayushtiwari5747462@gmail.com>
2020-06-15 18:50:54 +0530

    bug fixed

--- src/blockchain.js
+++ src/blockchain.js
@@ -85,7 +85,7 @@ class Blockchain {
      */
     requestMessageOwnershipVerification(address) {
         return new Promise((resolve) => {
-            resolve('${address}:${new Date().getTime().toString().slice(0,-3)}:starRegistry');
+            resolve(`${address}:${new Date().getTime().toString().slice(0,-3)}:starRegistry`);
         });
     }
 
